98b58748cbc3f605d6154da091039d3b021e781d
kishorekumarchokkakula <83263689+kishorekumarchokkakula@users.noreply.github.com>
2023-04-13 16:39:15 +0530

    Update query_employees.sh

--- query_employees.sh
+++ query_employees.sh
@@ -7,7 +7,7 @@ DB_PORT=3307
 DB_NAME="employees"
 
 # Set query
-QUERY="SELECT * FROM employees.employees"
+QUERY="SHOW databases;"
 
 # Connect to database and execute query
 mysql -u $DB_USER -p$DB_PASS -h $DB_HOST -P $DB_PORT -D $DB_NAME -e "$QUERY"
